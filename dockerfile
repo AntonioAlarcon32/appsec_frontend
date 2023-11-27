51f6f67312954b7ee7abe268f8a742aa8d84b798
Antonio Alarcon <antonio.alarcon@estudiantat.upc.edu>
2023-11-27 15:00:19 +0100

    Added dockerfile

---
+++ dockerfile
@@ -0,0 +1,29 @@
+# Stage 1: Build the React application
+FROM node:20 as build
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy package.json and package-lock.json (if available)
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy the rest of your app's source code
+COPY . .
+
+# Build the app
+RUN npm run build
+
+# Stage 2: Serve the app with Nginx
+FROM nginx:alpine
+
+# Copy the build output to replace the default nginx contents.
+COPY --from=build /app/build /usr/share/nginx/html
+
+# Expose port 80 to the outside once the container has launched
+EXPOSE 80
+
+# Start Nginx and keep it running
+CMD ["nginx", "-g", "daemon off;"]
--- src/pages/LoginForm.jsx
+++ src/pages/LoginForm.jsx
@@ -1,127 +1,118 @@
 import React, { useState } from 'react';
-import { TextField, Button, Container, Typography, Grid, Box, Link } from '@mui/material';
-import { Link as RouterLink } from 'react-router-dom'
+import { Card, TextField, Button, Container, Typography, Grid, Box, Link } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
 import GoogleButton from '../components/GoogleButton/GoogleButton.jsx';
 import { useNavigate } from 'react-router-dom';
-import axiosInstance from '../services/AxiosInstace.js';
-import {setAuthToken} from '../services/AxiosInstace.js'; 
+import axiosInstance, { setAuthToken } from '../services/AxiosInstace.js'; 
 import { useAuth } from '../AuthContext.jsx';
 
 const LoginForm = () => {
     const [formData, setFormData] = useState({
-      email: '',
-      password: '',
+        email: '',
+        password: '',
     });
-  
+
     const [emailError, setEmailError] = useState(false);
     const navigate = useNavigate();
     const { isLoggedIn, login } = useAuth();
-  
+
     const handleChange = (event) => {
-      setFormData({
-        ...formData,
-        [event.target.name]: event.target.value,
-      });
-  
-      // Validate email when the email field is changed
-      if (event.target.name === 'email') {
-        setEmailError(!validateEmail(event.target.value));
-      }
-    };
-  
-    const validateEmail = (email) => {
-      // Simple email validation pattern
-      const re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-      return re.test(String(email).toLowerCase());
-    };
-  
-    const handleSubmit = async (event) => {
-      event.preventDefault();
-      // Check for email validation before proceeding with the form submission
-      if (!validateEmail(formData.email)) {
-        setEmailError(true);
-        return; // Stop the form submission if email is not valid
-      }
-      try {
-        // Request to the backend to authenticate the user
-        const response = await axiosInstance.post('/auth/login', formData);
-    
-        // Validate the backend response
-        if (response.data && response.status === 200) {
-          // if succeded, navigate to the main page  
-          console.log('User authenticated:', response.data);
-          setAuthToken(response.data.token);
-          login();
-          navigate('/menu');
-        } else {
-          // Auth failed
-          console.error('Authentication error:', response.data || 'Did not get a valid response from the server.');
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value,
+        });
+
+        if (event.target.name === 'email') {
+            setEmailError(!validateEmail(event.target.value));
         }
-      } catch (error) {
-        console.error('Error while making login request:', error);
-        
-      }
     };
 
-  return (
-    <Container maxWidth="sm">
-      <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <Typography component="h1" variant="h3" gutterBottom>
-          Login
-        </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                value={formData.email}
-                onChange={handleChange}
-                error={emailError}
-                helperText={emailError ? "Please enter a valid email address" : ""}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                autoComplete="current-password"
-                value={formData.password}
-                onChange={handleChange}
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Login
-          </Button>
-          <Grid container justifyContent="center">
-            <Grid item>
-              <Link component={RouterLink} to="/register" variant="body2" sx={{ fontWeight: 'bold', color: 'primary.main' }}>
-                Don't have an account? Register
-              </Link>
-            </Grid>
-          </Grid>
-        </Box>
-      </Box>
-      <Box sx={{ my: 4, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                <GoogleButton />
-      </Box>
+    const validateEmail = (email) => {
+        const re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        return re.test(String(email).toLowerCase());
+    };
 
-    </Container>
-  );
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        if (!validateEmail(formData.email)) {
+            setEmailError(true);
+            return;
+        }
+        try {
+            const response = await axiosInstance.post('/auth/login', formData);
+    
+            if (response.data && response.status === 200) {
+                console.log('User authenticated:', response.data);
+                setAuthToken(response.data.token);
+                login();
+                navigate('/menu');
+            } else {
+                console.error('Authentication error:', response.data || 'Did not get a valid response from the server.');
+            }
+        } catch (error) {
+            console.error('Error while making login request:', error);
+        }
+    };
+
+    return (
+        <Container maxWidth="sm" sx={{ bgcolor: 'background.default', py: 5, height: '100vh' }}>
+            <Card elevation={3} sx={{ p: 3 }}>
+                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <Typography component="h1" variant="h4" gutterBottom>
+                        Login
+                    </Typography>
+                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    label="Email Address"
+                                    name="email"
+                                    autoComplete="email"
+                                    autoFocus
+                                    value={formData.email}
+                                    onChange={handleChange}
+                                    error={emailError}
+                                    helperText={emailError ? "Please enter a valid email address" : ""}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    value={formData.password}
+                                    onChange={handleChange}
+                                />
+                            </Grid>
+                        </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2, bgcolor: 'primary.main', '&:hover': { bgcolor: 'primary.dark' } }}
+                        >
+                            Login
+                        </Button>
+                        <Grid container justifyContent="center">
+                            <Grid item>
+                                <Link component={RouterLink} to="/register" variant="body2" sx={{ fontWeight: 'bold', color: 'primary.main' }}>
+                                    Don't have an account? Register
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+                <Box sx={{ mt: 4, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <GoogleButton />
+                </Box>
+            </Card>
+        </Container>
+    );
 };
 
 export default LoginForm;
